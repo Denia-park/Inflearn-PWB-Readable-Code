6441948829a4356bdb35d2d963a26c2aa3c6cd3c
hgpark <phg9587@naver.com>
2024-11-19 00:08:14 +0900

    Value Object

--- src/main/java/cleancode/minesweeper/tobe/BoardIndexConverter.java
+++ src/main/java/cleancode/minesweeper/tobe/BoardIndexConverter.java
@@ -1,33 +1,31 @@
 package cleancode.minesweeper.tobe;
 
-import static cleancode.minesweeper.tobe.Minesweeper.*;
-
 public class BoardIndexConverter {
 	private static final char BASE_CHAR_FOR_COL = 'a';
 
-	public int getSelectedColIndex(String cellInput, int colSize) {
+	public int getSelectedColIndex(String cellInput) {
 		char cellInputCol = cellInput.charAt(0);
-		return convertColFrom(cellInputCol, colSize);
+		return convertColFrom(cellInputCol);
 	}
 
-	private int convertColFrom(char cellInputCol, int colSize) {
+	private int convertColFrom(char cellInputCol) {
 		int colIndex = cellInputCol - BASE_CHAR_FOR_COL;
 
-		if(colIndex < 0 || colIndex >= colSize) {
+		if(colIndex < 0) {
 			throw new GameException("잘못된 입력입니다.");
 		}
 
 		return colIndex;
 	}
 
-	public int getSelectedRowIndex(String cellInput, int rowSize) {
+	public int getSelectedRowIndex(String cellInput) {
 		String cellInputRow = cellInput.substring(1);
-		return convertRowFrom(cellInputRow, rowSize);
+		return convertRowFrom(cellInputRow);
 	}
 
-	private int convertRowFrom(String cellInputRow, int rowSize) {
+	private int convertRowFrom(String cellInputRow) {
 		final int rowIndex = Integer.parseInt(cellInputRow) - 1; //index 값이기 때문에 -1을 빼준다
-		if(rowIndex < 0 || rowIndex >= rowSize) {
+		if(rowIndex < 0) {
 			throw new GameException("잘못된 입력입니다.");
 		}
 
